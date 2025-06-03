c792c5a4b49e26850b44eaa0a990c2f6290ca3c9
Saramoawad <saramoawad732@gmail.com>
2025-06-03 23:01:19 +0200

    productService,productInterface,displayProductInHome

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ProductsService } from '../../core/services/products.service';
+import { IProduct } from '../../core/interfaces/iproduct';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +8,22 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+//  private readonly _ProductsService= inject(ProductsService)
+ private readonly _ProductsService=inject(ProductsService)
+
+ productsList:IProduct[]=[] //will filled py products
+ ngOnInit(): void {
+   this._ProductsService.getAllProducts().subscribe({
+    next:(res)=>{
+      console.log(res.data);
+      this.productsList=res.data;
+    },
+    error:(err)=>{
+      console.log(err)
+    }
+   })
+ }
 
 }

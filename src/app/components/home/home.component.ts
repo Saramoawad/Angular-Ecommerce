2a2bba9dc962dbb210b36c0d8303456098fd8f1f
Saramoawad <saramoawad732@gmail.com>
2025-06-06 23:52:27 +0300

    add categories at slider using owl carousel package

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,13 +3,15 @@ import { ProductsService } from '../../core/services/products.service';
 import { IProduct } from '../../core/interfaces/iproduct';
 import { NgClass } from '@angular/common';
 import { Subscription } from 'rxjs';
-
+import { CategoriesService } from '../../core/services/categories.service';
+import { ICategory } from '../../core/interfaces/icategory';
+import {CarouselModule, OwlOptions} from 'ngx-owl-carousel-o'
 
 
 
 @Component({
   selector: 'app-home',
-  imports: [  NgClass ],
+  imports: [  NgClass, CarouselModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -17,10 +19,43 @@ export class HomeComponent implements OnInit ,OnDestroy{
 
 //  private readonly _ProductsService= inject(ProductsService)
  private readonly _ProductsService=inject(ProductsService)
+ private readonly _CategoriesService=inject(CategoriesService)
 
- productsList:IProduct[]=[] //will filled py products
+ productsList:IProduct[]=[]; //will filled py products
+categoriesList:ICategory[]=[];
  getAllProductsSub !: Subscription
+
+
+  customOptionsCat: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 6
+      }
+    },
+    nav: false
+  }
+
  ngOnInit(): void {
+  // get all product
  this.getAllProductsSub=  this._ProductsService.getAllProducts().subscribe({
     next:(res)=>{
       console.log(res.data);
@@ -30,6 +65,20 @@ export class HomeComponent implements OnInit ,OnDestroy{
       console.log(err)
     }
    })
+
+  //  get all cats
+  this._CategoriesService.getAllCategories().subscribe({
+    next:(res)=>{
+      console.log(res.data);
+      this.categoriesList=res.data;
+    },
+    error:(err)=>{
+      console.log(err)
+    }
+  })
+
+
+
  }
 
 // star rating
